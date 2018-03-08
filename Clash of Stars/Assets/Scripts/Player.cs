3f9a72b277c77ac6853ca2ac99be502a8e71ce30
mirkoemirbc <mirko@brightcrowns.com>
2018-03-07 22:08:05 -0300

    Network configuration
    - Added Network Manager
    - Added Network Id on PlayerPrefab
    - Created basic scene

---
+++ Clash of Stars/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+	public GameObject myCamera;
+
+	void Start () 
+	{
+		myCamera.SetActive (isLocalPlayer);
+
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
