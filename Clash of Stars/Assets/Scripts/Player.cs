adde2e2f856cb0c96689a8ef00f8f211008bd76e
mirkoemirbc <mirko@brightcrowns.com>
2018-04-21 22:27:42 -0300

    Cinemachine framework
    - Added Cinemachine library
    - Get out main camera from LocalPlayer prefab
    - Settings up virtual camera

--- Clash of Stars/Assets/Scripts/Player.cs
+++ Clash of Stars/Assets/Scripts/Player.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Player : NetworkBehaviour {
+public class Player : NetworkBehaviour 
+{
 
 	public GameObject myCamera;
+	private Cinemachine.CinemachineVirtualCamera myVirtualCamera;
 
-	void Start () 
+	void Awake () 
 	{
-		myCamera.SetActive (isLocalPlayer);
+		// First things first: We'll find the camera or use any camera already set.
+		// This only happens when the Player is LocalPlayer
+		if (isLocalPlayer == true)
+		{
+			if (myCamera == null)
+			{
+				myCamera = FindObjectOfType<Camera> ().transform.gameObject;
+			}
+			myCamera.SetActive (true);
+		}
 	}
-	
+
+	void Start ()
+	{
+		// Setting up some VirtualCamera only when Player is LocalPlayer
+		if (isLocalPlayer == true) 
+		{
+			myVirtualCamera = GameObject.FindObjectOfType<Cinemachine.CinemachineVirtualCamera> ();
+			if (DebugBehaviour.debuggingStatus == DebugTarget.CAMERA || DebugBehaviour.debuggingStatus == DebugTarget.ALL)
+				Debug.Log (myVirtualCamera);
+
+			myVirtualCamera.LookAt = transform.Find ("Camera LookAt Spot");
+			myVirtualCamera.Follow = transform.Find ("Camera Follow");
+		}
+	}
 }
